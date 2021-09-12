14ee386621274d18fa900178b96e242953d6f789
Tanvir Hossain Fahim <tanvirhossainfahim89@gmail.com>
2021-09-12 18:38:07 +0600

    solved

--- src/components/Home/UserData/UserData.js
+++ src/components/Home/UserData/UserData.js
@@ -32,7 +32,7 @@ const UserData = () => {
   const faArrowRight = <FontAwesomeIcon icon={faArrowAltCircleRight} />;
 
   useEffect(() => {
-    const url = "https://users-authentication.herokuapp.com/auth/all-users";
+    const url = "";
     axios.get(url).then((data) => {
       setAllUser(data.data);
       setLoading(true);
